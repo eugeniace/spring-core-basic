9bae139bcb4cd20ade0507670502657a11497870
eugenia.cemartan <eugenia.cemartan@itsmartsystems.eu>
2019-04-18 12:15:27 +0300

    ClassPathXmlApplicationContext example

--- src/main/java/com/iss/training/spring/DemoApplication.java
+++ src/main/java/com/iss/training/spring/DemoApplication.java
@@ -1,7 +1,15 @@
 package com.iss.training.spring;
 
+import com.iss.training.spring.beans.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class DemoApplication {
     public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/bundle-context.xml");
 
+        User user = ctx.getBean("user", User.class);
+
+        System.out.println("user = "+user);
     }
 }
